cdb53217f18435666450dc3e1df5e97d3a9fb60f
ofek kaufman <ofekkaufman1606@gmail.com>
2025-11-10 18:25:07 +0200

    הוספתי את כל הקבצים הנלווים והוספתי את Device.cpp

---
+++ Device.cpp
@@ -0,0 +1,51 @@
+﻿#include "Device.h"
+
+// אתחול המכשיר
+void Device::init(unsigned int id, DeviceType type, const std::string& os)
+{
+    m_id = id;
+    m_type = type;
+    m_os = os;
+    m_isActive = true; // כשנוצר מכשיר חדש, הוא פעיל כברירת מחדל
+}
+
+// איפוס המכשיר
+void Device::clear()
+{
+    m_id = 0;
+    m_type = PHONE;
+    m_os = "";
+    m_isActive = false;
+}
+
+// גטרים
+unsigned int Device::getID() const
+{
+    return m_id;
+}
+
+DeviceType Device::getType() const
+{
+    return m_type;
+}
+
+std::string Device::getOS() const
+{
+    return m_os;
+}
+
+bool Device::isActive() const
+{
+    return m_isActive;
+}
+
+// שינוי מצב
+void Device::activate()
+{
+    m_isActive = true;
+}
+
+void Device::deactivate()
+{
+    m_isActive = false;
+}
---
+++ Device.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <string>
+
+///////////////////////////////////////////////////////////
+/*                                                         //
+    Those definitions are used for tests,                //
+    you can add your own, but please do not delete any.  //
+*/                                                         //
+enum DeviceType { PHONE, PC, LAPTOP, TABLET };             //
+
+#define UbuntuOS "UbuntuLinux"                           
+#define RedHatOS "RedHatLinux"                            
+#define MacOS "MacOS"                                     
+#define IOS "IOS"                                         
+#define WINDOWS7 "Windows7"                               
+#define WINDOWS10 "Windows10"                             
+#define WINDOWS11 "Windows11"                             
+#define ANDROID "Android"                                 
+///////////////////////////////////////////////////////////
+
+class Device
+{
+private:
+    unsigned int m_id;
+    DeviceType m_type;
+    std::string m_os;
+    bool m_isActive;
+
+public:
+    void init(unsigned int id, DeviceType type, const std::string& os);
+    void clear();
+
+    unsigned int getID() const;
+    DeviceType getType() const;
+    std::string getOS() const;
+    bool isActive() const;
+
+    void activate();
+    void deactivate();
+};
